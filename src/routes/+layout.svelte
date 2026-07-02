5312c29b1f5bb8e806497f9c932377b0fe31d639
Liudmila Joukovskaya <liudmila.joukovskays@gmail.com>
2026-07-02 08:39:01 -0700

    Use logo for app icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -82,10 +82,14 @@
 			}
 		})();
 	</script>
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 	<link rel="manifest" href="/site.webmanifest" />
+	<meta name="application-name" content="Question Constellation" />
+	<meta name="apple-mobile-web-app-title" content="Question Constellation" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="theme-color" content="#eff8f8" media="(prefers-color-scheme: light)" />
 	<meta name="theme-color" content="#020617" media="(prefers-color-scheme: dark)" />
 </svelte:head>
